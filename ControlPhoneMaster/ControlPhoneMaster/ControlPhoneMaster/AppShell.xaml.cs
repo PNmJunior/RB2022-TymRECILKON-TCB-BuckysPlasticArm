a6d74dae6987604944760d3e120c79ea7b364e92
PNmJunior <nachajpavel@centrum.cz>
2022-05-04 15:59:40 +0200

    test0

---
+++ ControlPhoneMaster/ControlPhoneMaster/ControlPhoneMaster/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using ControlPhoneMaster.ViewModels;
+using ControlPhoneMaster.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace ControlPhoneMaster
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
---
+++ ControlPhoneMaster/ControlPhoneMaster/ControlPhoneMaster/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using ControlPhoneMaster.Models;
+using ControlPhoneMaster.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ControlPhoneMaster.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
