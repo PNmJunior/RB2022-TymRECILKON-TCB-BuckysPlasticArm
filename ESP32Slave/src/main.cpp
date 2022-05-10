f6f60bd64efb765cffaac71b83c8283d55b85cc9
PNmJunior <nachajpavel@centrum.cz>
2022-05-10 13:49:17 +0200

    Pridani schema se vsim

--- ESP32Slave/src/main.cpp
+++ ESP32Slave/src/main.cpp
@@ -2,6 +2,24 @@
 
 void setup() {
   // put your setup code here, to run once:
+  //H mustek
+  pinMode(32,OUTPUT);
+  pinMode(33,OUTPUT);
+  pinMode(25,OUTPUT);
+  pinMode(26,OUTPUT);
+  pinMode(27,OUTPUT);
+  pinMode(13,OUTPUT);
+  pinMode(23,OUTPUT);
+  pinMode(19,OUTPUT);
+
+//posuvny registr
+  pinMode(18,OUTPUT);
+  pinMode(17,OUTPUT);
+  pinMode(16,OUTPUT);
+
+  //dotikovy senzor
+  touchRead(2);
+  touchRead(3);
 }
 
 void loop() {
