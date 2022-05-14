3496f32431642f6779a0ed1d8b75e063843adb59
PNmJunior <nachajpavel@centrum.cz>
2022-05-14 17:40:13 +0200

    Dokončení hardwaru
    Dokončeni hardwaru bylo hodně náročné na slepé kontakty u propojovacích kabelů. Elektronika je nyní uzavřena v bývalém boxu pro baterie, protože kabely se tam při uzavíraní nevešly. Musel jsem nadzvednout horní stěnu a tím pádem jsem vyřešil i problém s chlazením. Baterie od starého notebooku je schovaná v podvozku robota.

--- ESP32Slave/src/main.cpp
+++ ESP32Slave/src/main.cpp
@@ -4,7 +4,7 @@
 #include <WebServer.h>
 #include <ESPmDNS.h>
 #include <Update.h>
-#include <tajnosti.h>
+#include <tajnosti.h>//Jsou zde definované jmého a heslo wifi
 #include <webHTMLtext.h>
 
 const char* host = "BuckysPlasticArm";
@@ -21,11 +21,10 @@ WebServer server(80);
 #define Mpocet 8
 int Mpiny[Mpocet] = {32,33,23,19,27,13,26,25};//bila-modra,cerv-fial,zel-bila,zlut-seda,fial-bila,mod-fial,fial-bila,seda-cerv,
 byte Mdata = 0;
-//32-bila-modra,33-cerv-fial,23-zel-bila,19-zlut-seda,27-fial-bila,13-mod-fial,26-fial-bila,25-seda-cerv,
+//32-bila-modra,33-cerv-fial,23-zel-bila,19-zlut-seda,27-fial-bila,13-mod-fial,26-seda-cerv,25-bila-zel
 #define MStopLow 0
 #define MStopHight 10
 #define MStop MStopLow
-//26-seda-cerv,25-bila-zel
 #define Mvpred 1
 #define Mvzad -1
 #define MEindex -1
@@ -35,19 +34,53 @@ int Mresolution = 8;
 int MlatchPin = 18;
 int MdataPin = 17;
 int MclockPin = 16;
+float MprocDataIn[8][2];
+float MprocDataWork[8][2];
 
-//test2=test motorů
+//definice indexu motorů
+#define MotPodLivi 1
+#define MotPodPravi 0
+#define Mot1 6
+#define Mot2 2
+#define Mot3 3
+#define Mot4 5
+#define MotKleste 4
+#define LEDka 7
+
+//Negace směru motorů
+#define MotNegPodLivi 0
+#define MotNegPodPravi 1
+#define MotNeg1 0
+#define MotNeg2 1
+#define MotNeg3 0
+#define MotNeg4 1
+#define MotNegKleste 0
+#define LEDkaNeg 0
+
+int MotorNegace[8] = {MotNegPodPravi,MotNegPodLivi, MotNeg2,MotNeg3,MotNegKleste, MotNeg4, MotNeg1,LEDkaNeg };
+
+//Ostatní definice k motorům
+#define LEDmod Mvpred
+#define KlesSevrit Mvzad
+#define KlesRozevrit Mvpred
+#define Mot1Vlevo Mvzad
+#define Mot1Vprevo Mvpred
+#define Dolu Mvpred
+#define Nahoru Mvzad
+
+//test2 motorů
 long timeTest1=0;
 byte mmm=0;
 
-void PosunReg()
+void PosunReg()//odešle data k posuvnému registru
 {
     digitalWrite(MlatchPin, LOW);
     shiftOut(MdataPin, MclockPin, MSBFIRST, Mdata);
     digitalWrite(MlatchPin, HIGH);  
 }
 
-void PosunReg(byte dataNov )
+
+void PosunReg(byte dataNov )//Pokud nastane změna výstupu posuvného registru, pošlou se data
 {
   if (dataNov != Mdata)
   {
@@ -57,7 +90,7 @@ void PosunReg(byte dataNov )
 }
 
 
-void PosRegIndex(int index, bool A_N)
+void PosRegIndex(int index, bool A_N)//Vyrobení změny ve výstupu pos. registru
 {
   if (A_N)
   {
@@ -69,7 +102,8 @@ void PosRegIndex(int index, bool A_N)
   }
 }
 
-void MotorStopAll()
+
+void MotorStopAll()//Vždy zastaví chod robota. Automaticky posílá data posuvnému registru.
 {
   Mdata = 0;
   PosunReg();
@@ -77,26 +111,30 @@ void MotorStopAll()
   {
     ledcWrite(i, 0);
   }
-  
 }
 
-void MotorBegin(int Platch, int Pdata, int Pclock, int piny[Mpocet])
-{
-MlatchPin =Platch;
-MdataPin = Pdata;
-MclockPin = Pclock;
 
-for (int i = 0; i < Mpocet; i++)
+void MotorSetProc(int index, byte minimum, byte maximum)// nastavení MotorRunProc
 {
-  Mpiny[i] = piny[i];
-        ledcSetup(i, Mfreq, Mresolution);
-  
-  // attach the channel to the GPIO to be controlled
-  ledcWrite(i, 255);
-  //ledcAttachPin(Mpiny[i], i);
-
+  MprocDataIn[index][0] = minimum;
+  MprocDataIn[index][1] = maximum;
+  MprocDataWork[index][0] = MprocDataIn[index][0] ;
+  MprocDataWork[index][1] = MprocDataIn[index][1]  -MprocDataIn[index][0] ;
 }
 
+
+void MotorBegin(int Platch, int Pdata, int Pclock, int piny[Mpocet])//inicializace potřebných zaležitostí k motorům
+{
+  MlatchPin =Platch;
+  MdataPin = Pdata;
+  MclockPin = Pclock;
+
+  for (int i = 0; i < Mpocet; i++)
+  {
+    Mpiny[i] = piny[i];
+    ledcSetup(i, Mfreq, Mresolution);
+    ledcWrite(i, 255);
+  }
   pinMode(MlatchPin,OUTPUT);
   pinMode(MdataPin,OUTPUT);
   pinMode(Pclock,OUTPUT);
@@ -108,10 +146,14 @@ for (int i = 0; i < Mpocet; i++)
     pinMode(Mpiny[i],OUTPUT);
     ledcAttachPin(Mpiny[i], i);
   }
+    for (int i = 0; i < Mpocet; i++)
+  {
+    MotorSetProc(i,0,255);
+  }
 }
 
 
-void MotorRun(int index, int mod, int rychlost)
+void MotorRunRaw(int index, int mod=MStop, int rychlost =0)
 {
   if (rychlost < 0)
   {
@@ -121,6 +163,18 @@ void MotorRun(int index, int mod, int rychlost)
   {
     rychlost = 255;
   }
+  if (MotorNegace[index]==1)
+  {
+    if (mod == Mvpred)
+    {
+      mod = Mvzad;
+    }
+    else if (mod == Mvzad)
+    {
+      mod = Mvpred;
+    }
+  }
+  
   if (mod == MStopLow)
   {
     ledcWrite(index, 0);
@@ -141,77 +195,73 @@ void MotorRun(int index, int mod, int rychlost)
     ledcWrite(index,255- rychlost);
     PosRegIndex(index, 1);
   }
-  //Serial.print(index);Serial.print(",");Serial.print(Mpiny[index]);Serial.print(",");Serial.print(mod);Serial.print(","),Serial.println(rychlost);
+}
+
+
+void MotorRunProc(int index, float proc)//Možnost nastavení procent výkonu, kde je ovládaný reálný výkon motoru...
+{
+  if(proc == 0.0)
+  {
+    MotorRunRaw(index,MStop);
+  }
+  else
+  {
+    float o  =(abs(proc)*MprocDataWork[index][1]/100.0)+MprocDataWork[index][0];
+    int u = o;
+    if (u >255)
+    {
+      u = 255;
+    }
+    else if (u < 0)
+    {
+      u = 0;
+    }
+    if (proc < 0.0)
+    {
+      MotorRunRaw(index,Mvzad, u);
+    }
+    else
+    {
+      MotorRunRaw(index, Mvpred, u);
+    }
+  }
 }
 
 
 void setup() {
   // put your setup code here, to run once:
-  //H mustek
-  /*
-  pinMode(32,OUTPUT);
-  pinMode(33,OUTPUT);
-  pinMode(25,OUTPUT);
-  pinMode(26,OUTPUT);
-  pinMode(27,OUTPUT);
-  pinMode(13,OUTPUT);
-  pinMode(23,OUTPUT);
-  pinMode(19,OUTPUT);
-
-//posuvny registr
-  pinMode(18,OUTPUT);
-  pinMode(17,OUTPUT);
-  pinMode(16,OUTPUT);
-*/
-
 MotorBegin(18,17,16,Mpiny);
-  //dotikovy senzor
-  //touchRead(2);
-  //touchRead(3);
-
-//inicializace PWM
-  // configure LED PWM functionalitites
-  /*
-  for (size_t i = 0; i < Mpocet; i++)
-  {
-      ledcSetup(i, Mfreq, Mresolution);
-  
-  // attach the channel to the GPIO to be controlled
-  ledcAttachPin(Mpiny[i], i);
-  //Serial.print(i);Serial.print(",");Serial.println(Mpiny[i]);
-  
-  }
 
   MotorStopAll();
-  */
-  //Serial.begin(9600);
-  //Inicializace OTA
-  
+  //dotykový senzor
+  touchRead(2);
+  touchRead(3);
 
+  Serial.begin(9600);
+  //Inicializace OTA
   // Connect to WiFi network
   WiFi.begin(ssid, password);
-  //Serial.println("");
+  Serial.println("");
 
   // Wait for connection
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
-    //Serial.print(".");
+    Serial.print(".");
   }
-  /* 
   Serial.println("");
   Serial.print("Connected to ");
   Serial.println(ssid);
   Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());*/
+  Serial.println(WiFi.localIP());
 
   /*use mdns for host name resolution*/
   if (!MDNS.begin(host)) { //http://esp32.local
-    //Serial.println("Error setting up MDNS responder!");
+    Serial.println("Error setting up MDNS responder!");
     while (1) {
       delay(1000);
     }
   }
-  //Serial.println("mDNS responder started");
+  Serial.println("mDNS responder started");
   /*return index page which is stored in serverIndex */
   server.on("/", HTTP_GET, []() {
     server.sendHeader("Connection", "close");
@@ -240,15 +290,13 @@ MotorBegin(18,17,16,Mpiny);
       }
     } else if (upload.status == UPLOAD_FILE_END) {
       if (Update.end(true)) { //true to set the size to the current progress
-        //Serial.printf("Update Success: %u\nRebooting...\n", upload.totalSize);
+        Serial.printf("Update Success: %u\nRebooting...\n", upload.totalSize);
       } else {
         Update.printError(Serial);
       }
     }
   });
   server.begin();
-
-  //MotorRun(0,Mvpred,(int)255);
 }
 
 
@@ -257,22 +305,22 @@ void loop(void) {
   delay(1);
   timeTest1 ++;
   //byte mmm=0;
-  /*
-  if (timeTest1 == 250)
+  //test
+  if (timeTest1 == 500)
   {
-    MotorRun(mmm,Mvpred,(int)255);
-  }
-    if (timeTest1 == 500)
-  {
-    MotorRun(mmm,MStopLow,(int)255);
-  }
-    if (timeTest1 == 7500)
-  {
-    MotorRun(mmm,Mvzad,(int)255);
+    MotorRunRaw(mmm,Mvpred,(int)150);
   }
     if (timeTest1 == 1000)
   {
-    MotorRun(mmm,MStopHight,(int)255);
+    MotorRunRaw(mmm,MStopLow,(int)150);
+  }
+    if (timeTest1 == 1500)
+  {
+    MotorRunRaw(mmm,Mvzad,(int)150);
+  }
+    if (timeTest1 == 2000)
+  {
+    MotorRunRaw(mmm,MStopHight,(int)150);
   
     if (mmm == 7)
     {
@@ -282,53 +330,8 @@ void loop(void) {
     {
       mmm++;
     }
+    timeTest1=0;
     
-  }*/
-
-
-/*
-
-    if (timeTest1 == 1000)
-  {
-    MotorRun(mmm,Mvpred,(int)255);
-  }
-    if (timeTest1 == 2000)
-  {
-    MotorRun(mmm,MStopLow,(int)255);
-  }
-    if (timeTest1 == 3000)
-  {
-    MotorRun(mmm,Mvzad,(int)255);
-  }
-    if (timeTest1 == 4000)
-  {
-    MotorRun(mmm,MStopHight,(int)255);
   }
 
-  if (timeTest1 == 5000)
-  {
-    MotorRun(mmm,Mvpred,(int)100);
-  }
-    if (timeTest1 == 6000)
-  {
-    MotorRun(mmm,MStopLow,(int)100);
-  }
-    if (timeTest1 == 7000)
-  {
-    MotorRun(mmm,Mvzad,(int)100);
-  }
-    if (timeTest1 == 8000)
-  {
-    MotorRun(mmm,MStopHight,(int)100);
-    timeTest1 = 0;
-    if (mmm == 7)
-    {
-      mmm=0;
-    }
-    else
-    {
-      mmm++;
-    }
-    
-  }*/
 }
